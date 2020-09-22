808bd458793eb6b575d7637d39be50896b0f5ae7
于军振 <yujunzheno@163.com>
2020-09-22 22:16:23 +0800

    first

---
+++ FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.firstproject;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout frameLayout = new FrameLayout(this);
+        frameLayout.setBackgroundResource(R.mipmap.qr);
+        setContentView(frameLayout);
+        TextView textView = new TextView(this);
+        textView.setText("开始游戏");
+        textView.setTextColor(Color.rgb(100,10,30));
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER;
+        textView.setLayoutParams(params);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void  onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this).setTitle("系统提示").setMessage("游戏又风险").setPositiveButton("确定",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                Log.i("heheeeheh");
+                            }
+                        }).setNegativeButton("退出", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Log.i("1234");
+                        finish();
+                    }
+                }).show();
+            }
+        });
+        frameLayout.addView(textView);
+    }
+}
