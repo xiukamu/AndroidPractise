f44831f8770cc2d339088a7bad190f9c226c9be5
于军振 <yujunzheno@163.com>
2020-09-28 20:53:11 +0800

    练习相对布局

---
+++ FirstProject/app/src/main/java/com/example/firstproject/RelativeTest.java
@@ -0,0 +1,13 @@
+package com.example.firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_test);
+    }
+}
